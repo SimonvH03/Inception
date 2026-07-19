3b379e08a785d6afa587c8707e3a224b5466c764
simon <svan-hoo@inception.codam.nl>
2026-07-19 12:27:46 -0400

    wow stuff is worken

--- src/wordpress/init_php.sh
+++ src/wordpress/init_php.sh
@@ -5,14 +5,14 @@ install() {
 
 	wp core download
 	wp config create \
-		--dbname="${WP_DB_NAME}" \
-		--dbuser="${WP_ADMIN_USER}" \
-		--dbpass="${WP_ADMIN_PASSWORD}" \
+		--dbname="${DB_NAME}" \
+		--dbuser="${DB_USER}" \
+		--dbpass="${DB_USER_PASSWORD}" \
 		--dbhost="mariadb:3306"
 	wp core install \
 		--url=localhost \
 		--title=Inception \
-		--admin_user="${WP_ADMIN_USER}" \
+		--admin_user="${WP_ADMIN}" \
 		--admin_password="${WP_ADMIN_PASSWORD}" \
 		--admin_email="${WP_ADMIN_MAIL}" \
 		--skip-email
@@ -21,6 +21,17 @@ install() {
 		"${WP_USER}" \
 		"${WP_USER_MAIL}"
 }
+
+
+until mariadb \
+    -h mariadb \
+    -u "$DB_USER" \
+    -p"$DB_USER_PASSWORD" \
+    -e "SELECT 1;" >/dev/null 2>&1
+do
+    echo "Waiting for MariaDB..."
+    sleep 2
+done
 
 if [ ! -f /var/www/html/index.php ]; then
     echo "initialising wordpress..."
